ec8b032f319e45d071035575bd9f2fd7fc7ce395
Alejandra Camacho <camacho.ale94@gmail.com>
2020-10-27 00:36:38 -0600

    Components created: Heroes & About

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component'
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component'
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, AboutComponent, HeroesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.styl']
+})
+export class HeroesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
