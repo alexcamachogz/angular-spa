95f50aa3eee0a82186d7d86f5102f6b73e41e8a8
Alejandra Camacho <camacho.ale94@gmail.com>
2020-10-26 23:05:47 -0600

    Create home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
-import { NavbarComponent } from './components/shared/navbar/navbar.component'
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component'
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
