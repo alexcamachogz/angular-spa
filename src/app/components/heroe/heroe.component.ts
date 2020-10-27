270ffb3cb434219aeb0b229ac83d034dc0b85848
Alejandra Camacho <camacho.ale94@gmail.com>
2020-10-27 14:06:01 -0600

    Service created to get only one heroe, according with url params

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core'
-
+import { Component } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { HeroesService } from '../../services/heroes.service'
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
 })
-export class HeroeComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class HeroeComponent {
+  heroe = {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService
+  ) {
+    this.activatedRoute.params.subscribe((params) => {
+      this.heroe = this.heroesService.getHeroe(params.id)
+    })
+  }
 }
